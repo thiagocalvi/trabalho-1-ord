472b0329c3bdf8848cc89f2b78b76637e27c46c4
thiagocalvi <thiagohcalvi@gmail.com>
2024-07-04 16:28:06 -0300

    Organizando

--- GerenciadorLED.py
+++ GerenciadorLED.py
@@ -1,14 +1,1 @@
 class GerenciadorLED:
-    def __init__(self):
-        pass
-
-    def imprimirLED(self):
-        print("LED")
-
-    def ordenarLED(self):
-        #Ordena a LED utilizando worst-fit
-        return 's'
-
-    def buscarEspacoLED(self):
-        #busca na led o melhor espaço disponivel
-        return 's'
