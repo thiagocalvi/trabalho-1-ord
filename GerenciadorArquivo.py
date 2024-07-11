8fa5994f831e3320d6183f9613fa360e5547b4d4
thiagocalvi <thiagohcalvi@gmail.com>
2024-07-11 09:01:46 -0300

    Métodos do gerenciadorLED foram testados, tudo OK!

--- GerenciadorArquivo.py
+++ GerenciadorArquivo.py
@@ -1,3 +1,4 @@
+import struct
 class GerenciadorArquivo:
     def __init__(self, path_file:str, operations_file:str, header_size=4, record_size_field=2, min_size_fragmentation=10) -> None:
         self.file = path_file
@@ -18,26 +19,27 @@ class GerenciadorArquivo:
         self.file.close()
         self.operations_file.close()
     
-    def tamanhoRegistro(self, byteOffset) -> int:
-        self.file.seek(byteOffset)
+    #Finalizado e testado
+    def tamanhoRegistro(self, offset) -> int:
+        self.file.seek(offset)
         tam_registro = int.from_bytes(self.file.read(self.RECORD_SIZE_FIELD))
         return tam_registro
     
+    #Pode ser não utilizado
     def resetPonteiro(self) -> None:
         self.file.seek(0)
 
+    #Pode ser não utilizado
     def lerCabecalho(self) -> int:
         self.resetPonteiro()
-        cabecalho = int.from_bytes(self.file.read(self.HEADER_SIZE))
-        return cabecalho
-
+        return struct.unpack("I", self.file.read(self.HEADER_SIZE))[0]
+    
+    #Pode ser não utilizado
     def escreverCabecalho(self, offset:int) -> None:
-        if type(offset) != bytes:
-            offset = offset.to_bytes(self.HEADER_SIZE)
-
         self.resetPonteiro()
-        self.file.write(offset)
+        self.file.write(struct.pack('I', offset))
 
+    #Finalizado e testado
     def buscarRegistro(self, indenficador:int):
         
         offset = self.HEADER_SIZE
@@ -67,42 +69,58 @@ class GerenciadorArquivo:
         
         return "Registro não encontrado"
 
+
+    #Finalizado e não testado, na teoria é para funcionar
     def inserirRegistro(self, dados_registro:str):
         
         buffer = dados_registro.encode()
-        tam_registro = len(buffer) + self.RECORD_SIZE_FIELD
-        offset_registro_LED = self.lerCabecalho()
+        tam_registro = len(buffer)
+        self.file.seek(0)
+
+        offset_registro_LED = self.file.read(self.HEADER_SIZE)
+
+        if offset_registro_LED == b'\xff\xff\xff\xff':
+            #Inserir registro no final do arquivo
+            self.file.seek(0, 2)
+            self.file.write(tam_registro.to_bytes(self.RECORD_SIZE_FIELD))
+            self.file.write(buffer)
+            return "Registro enserido no final do arquivo"
+
+
+        offset_registro_LED = struct.unpack("I", offset_registro_LED)[0]
         tam_registro_LED = self.tamanhoRegistro(offset_registro_LED)
 
         if tam_registro == tam_registro_LED:
+            #Inserir o registor no primeiro espaço disponivel na led os dois
+            #registros tem mesmo tamanho
             self.file.seek(offset_registro_LED+self.RECORD_SIZE_FIELD)
             self.file.write(buffer)
         
         elif tam_registro < tam_registro_LED:
-
+            #Inserir o registor no primeiro espaço disponivel na led
+            #novo registro é menor e gera um no espaço livre
             self.file.seek(offset_registro_LED)
             self.file.write(tam_registro.to_bytes(self.RECORD_SIZE_FIELD))
+            self.file.write(buffer)
             
             offset_fragmentacao = offset_registro_LED + tam_registro + self.RECORD_SIZE_FIELD
-            tam_fragmentacao = tam_registro_LED - (tam_registro - self.RECORD_SIZE_FIELD)
+            tam_fragmentacao = tam_registro_LED - tam_registro
 
-            self.file.write(buffer)
             
+            self.file.seek(offset_fragmentacao)
+            n_buffer = n_buffer.ljust(tam_fragmentacao + self.RECORD_SIZE_FIELD, b'\0')
+            self.file.write(n_buffer)
+
             if tam_fragmentacao >= self.MIN_SIZE_FRAGMENTATION:
                 self.file.seek(offset_fragmentacao)
-                n_buffer = n_buffer.ljust(tam_fragmentacao - self.RECORD_SIZE_FIELD, b'\0')
                 self.file.write(tam_fragmentacao.to_bytes(self.RECORD_SIZE_FIELD))
-                self.file.write(n_buffer)
                 self.inserirEspacoLED(offset_fragmentacao, tam_fragmentacao)
 
-        else:
-            self.file.seek(0, 2)
-            self.file.write(tam_registro.to_bytes(self.HEADER_SIZE))
-            self.file.write(buffer)
 
+    #Finalizado e testado
     def removerRegistro(self, identificador):
         offset, tam_registro = self.buscarRegistro(identificador)[-2:]
-        self.file.seek(offset+self.RECORD_SIZE_FIELD)
+        self.file.seek(offset+2)
         char_remocao = "*".encode()
         self.file.write(char_remocao)
         self.inserirEspacoLED(offset, tam_registro)
@@ -140,71 +158,105 @@ class GerenciadorArquivo:
         return "Fim das operações"
 
 
+ 
     #GerenciadorLED
-    #ainda não está funcionando corretemente
-    def inserirEspacoLED(self, offset_novo_espaco:int, tam_novo_espaco:int) -> None:
-        offset_LED = self.lerCabecalho()
 
-        if offset_LED == -1:
-            self.file.seek(offset_novo_espaco+3)
-            self.file.write(offset_LED.to_bytes(self.HEADER_SIZE))
-            self.escreverCabecalho(offset_novo_espaco)
-
-        else:
-            while offset_LED != -1:
+    #Finalizado e testado
+    def inserirEspacoLED(self, offset_novo_espaco:int, tam_novo_espaco:int) -> str:
+            self.file.seek(0)
+            cabecalho = self.file.read(4)
+            end_LED = b'\xff\xff\xff\xff'
+            
+            #Caso 1: LED está vazia
+            if cabecalho == end_LED:
+                self.file.seek(0)
+                self.file.write(struct.pack("I", offset_novo_espaco))
+                self.file.seek(offset_novo_espaco+3)
+                self.file.write(end_LED)
+                return "Caso 1: LED está vazia"
+            
+            else:
+                offset_LED = struct.unpack("I", cabecalho)[0]
                 self.file.seek(offset_LED)
-                tam_atual = int.from_bytes(self.file.write(self.RECORD_SIZE_FIELD))
+                #tam_espaco_LED = struct.unpack("H", self.file.read(2))[0]
+                tam_espaco_LED = int.from_bytes(self.file.read(2))
                 self.file.seek(1,1)
-                next_offset = int.from_bytes(self.file.write(self.HEADER_SIZE))
+                next_offset_LED = self.file.read(4)
 
-                if tam_novo_espaco >= tam_atual and next_offset == -1:
+                #Caso 2: O novo espaço é maior que o primeiro espaço da LED
+                if tam_novo_espaco >= tam_espaco_LED:
+                    self.file.seek(0)
+                    self.file.write(struct.pack("I", offset_novo_espaco))
                     self.file.seek(offset_novo_espaco+3)
-                    self.file.write(offset_LED.to_bytes(self.HEADER_SIZE))
-                    self.escreverCabecalho(offset_novo_espaco)
-                    break
+                    self.file.write(struct.pack("I", offset_LED))
+                    return "Caso 2: O novo espaço é maior que o primeiro espaço da LED"
 
-                self.file.seek(next_offset)
-                tam_next_offset = int.from_bytes(self.file.seek(self.HEADER_SIZE))
 
-                if tam_novo_espaco >= tam_next_offset:
+                offset_LED = cabecalho
+
+                #Caso 3: O novo espaço será inserido no meio da LED
+                while next_offset_LED != end_LED:
+                    self.file.seek(struct.unpack("I", next_offset_LED)[0])
+                    tam_next_offset = int.from_bytes(self.file.read(2))
+
+                    if tam_novo_espaco >= tam_next_offset:
+                        self.file.seek(offset_novo_espaco+3)
+                        self.file.write(next_offset_LED)
+                        self.file.seek(struct.unpack("I", offset_LED)[0]+3)
+                        self.file.write(struct.pack("I", offset_novo_espaco))
+                        return "Caso 3: O novo espaço será inserido no meio da LED"
+
+                    offset_LED = next_offset_LED
+                    self.file.seek(struct.unpack("I", offset_LED)[0]+3)
+                    next_offset_LED = self.file.read(4)
+
+                if next_offset_LED == end_LED:
+                    #Caso 4: O novo espaço será inserido no final da LED
+                    self.file.seek(struct.unpack("I", offset_LED)[0]+3)
+                    self.file.write(struct.pack("I", offset_novo_espaco))
                     self.file.seek(offset_novo_espaco+3)
-                    self.file.write(next_offset.to_bytes(self.HEADER_SIZE))
-                    self.file.seek(offset_LED)
-                    self.file.write(offset_novo_espaco.to_bytes(self.HEADER_SIZE))
-                    break
-    
+                    self.file.write(end_LED)
+                    return "Caso 4: O novo espaço será inserido no final da LED"
+
+    #Finalizado e testado
     def removerEspacoLED(self) -> None:
 
-        cabeca_led:int = self.lerCabecalho()
+        self.file.seek(0)
+        offset = self.file.read(self.HEADER_SIZE)
 
-        if cabeca_led != -1:
-            self.file.seek(cabeca_led)
-            self.seek(3, 1)
-            nova_cabeca:bytes = self.file.read(self.HEADER_SIZE)
-            self.escreverCabecalho(nova_cabeca)
+        if offset != b'\xff\xff\xff\xff':
+            offset = struct.unpack("I", offset)[0]
+            self.file.seek(offset)
+            self.file.seek(3, 1)
+            nova_cabeca = self.file.read(self.HEADER_SIZE)
+            self.file.seek(0)
+            self.file.write(nova_cabeca)
 
             return "Espaço removido!"
         
         else:
             return "A LED está vazia!"
 
+    #Finalizado e testado
     def imprimirLED(self) -> None:
         # LED -> [offset: 4, tam: 80] -> [offset: 218, tam: 50] -> [offset: 169, tam: 47] -> [offset: -1]
         # Total: 3 espaços disponíveis
         
         lista: list[list[int]] = []
-        offset: int = self.lerCabecalho()
+        self.file.seek(0)
+        offset = self.file.read(self.HEADER_SIZE)
         
-        if offset == -1:
+        if offset == b'\xff\xff\xff\xff':
             return "LED vazia"
         
-        while offset != -1:
-            print(offset)
+        while offset != b'\xff\xff\xff\xff':
+            offset = struct.unpack("I", offset)[0]
             self.file.seek(offset)
             tam = int.from_bytes(self.file.read(self.RECORD_SIZE_FIELD))
+            self.file.seek(1, 1)
             lista.append([offset, tam])
             next_offset = self.file.read(self.HEADER_SIZE)
-            offset = int.from_bytes(next_offset, signed=True)
+            offset = next_offset
         
         # Imprimir a lista de offsets e tamanhos
         for x in lista:
@@ -213,38 +265,22 @@ class GerenciadorArquivo:
         print("[offset: -1]")
         print(f"\nTotal: {self.tamanhoLED()} espaços disponíveis")
 
+    #Finalizada e testado
     def tamanhoLED(self) -> int:
         tam_LED:int = 0
-        offset:int = self.lerCabecalho()
+        self.file.seek(0)
+        offset = self.file.read(self.HEADER_SIZE)
 
-        while offset != -1:
+        while offset != b'\xff\xff\xff\xff':
+            offset = struct.unpack("I", offset)[0]
             self.file.seek(offset+3)
-            offset:int = int.from_bytes(self.file.read(self.HEADER_SIZE))
+            offset = self.file.read(self.HEADER_SIZE)      
             tam_LED += 1
 
         return tam_LED
 
 
-
-
-
-
-
-a = GerenciadorArquivo("3.dat", "operacao.txt")
+a = GerenciadorArquivo("1.dat", "operacao.txt")
 a.abirArquivo()
-# print(a.lerArquivoOperacoes())
-# print(a.lerArquivoOperacoes())
 
-#print(a.buscarRegistro(1))
-#print(a.buscarRegistro(2))
-#print(a.buscarRegistro(3))
-
-a.removerRegistro(1)
-
-a.removerRegistro(2)
-
-a.removerRegistro(3)
-
-# a.initCabecalho()
-# a.imprimirLED()
 a.fecharArquivo()
